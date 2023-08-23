2e162ac72a5a17422f40a576f4c28e39159ddc33
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2023-08-24 00:14:56 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if (len(items) == 1):
+        return ''.join(items)
+    elif (len(items)) == 2:
+        return ' and '.join(items)
+    elif (len(items) == 3):
+        comma_seperated = ', '.join(items[:-1])
+        return f"{comma_seperated}, and {items[-1]}"
+    elif (len(items) > 3):
+        comma_seperated = ', '.join(items[:-1])
+        return f"{comma_seperated}, and {items[-1]}"
+
+
+print(oxford_comma(["Hello", "kiwi", "starfruid"]))
